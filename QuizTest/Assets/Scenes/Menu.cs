0a87d9f33338c4a42b89486f2bb98f33a539258b
Stefannsen <stefan.negulescu@my.fmi.unibuc.ro>
2021-06-10 18:31:46 +0300

    Update 3

---
+++ QuizTest/Assets/Scenes/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PLayGame()
+    {
+        SceneManager.LoadScene("Scene_1");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Scene_home");
+    }
+
+    public void LoadOptionsMenu()
+    {                         
+        SceneManager.LoadScene("Scene_OptionsMenu");
+    }
+
+    public void LoadPlayMenu()
+    {
+        SceneManager.LoadScene("Scene_PlayMenu");
+    }
+
+
+}
