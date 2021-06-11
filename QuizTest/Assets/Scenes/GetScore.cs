c8ba50a10501342994eb79afcf956202d78db547
hirneagabriel <hirneagabriel@users.noreply.github.com>
2021-06-11 13:23:47 +0300

    bug fixes

---
+++ QuizTest/Assets/Scenes/GetScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GetScore : MonoBehaviour
+{
+    int score = QuizManager.getScore();
+     [SerializeField]
+    private Text Score;
+     void Start ()
+    {
+        Score.text = score.ToString();
+        QuizManager.setScore();
+
+    }
+}
